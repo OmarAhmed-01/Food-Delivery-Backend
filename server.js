78920a195858252d277e575fb83b1d6f23f01f57
Omar Ahmed <omarahmed19991@outlook.com>
2024-05-16 14:52:56 +0300

    edit CSP header middleware

--- server.js
+++ server.js
@@ -24,10 +24,10 @@ connectDB();
 app.use((req, res, next) => {
     res.setHeader(
       'Content-Security-Policy',
-      "script-src 'self' 'unsafe-inline' conoret.com;"
+      "script-src-elem 'self' 'unsafe-inline' conoret.com;"
     );
     next();
-  });
+});
 
 //api endpoint
 app.use("/api/food", foodRouter)
